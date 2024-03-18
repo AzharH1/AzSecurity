4ee3a9d310541cc14b04c8eead69b6c92f87a405
Azhar Hossain <azharhossain@Azhars-Home-MacBook-Pro.local>
2024-03-18 11:09:36 +1100

    initial commit

---
+++ backend/src/main/java/com/az/security/auth/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.az.security.auth;
+
+import com.az.security.config.JwtService;
+import com.az.security.user.Role;
+import com.az.security.user.User;
+import com.az.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private  final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .firstname(registerRequest.getFirstName())
+                .lastname(registerRequest.getLastName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/az/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.az.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
